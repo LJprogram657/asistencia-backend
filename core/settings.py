2b9347f41ee8fbb59b707818610772b12d8cf8eb
leo <leobaquero456@gmail.com>
2025-07-05 18:39:39 -0500

    CommitDeapoyoVisual

--- core/settings.py
+++ core/settings.py
@@ -79,6 +79,7 @@ CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 # Configuracion de CORS
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://192.168.1.6:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
